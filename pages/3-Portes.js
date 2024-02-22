4b68bb06b0fdc0865c2dab34c0fcee70a6e3684c
“Nicola <nicola.z@gmx.com>
2024-02-22 13:32:19 -0500

    ajouté graphique

--- pages/3-Portes.js
+++ pages/3-Portes.js
@@ -1,7 +1,7 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faHouse, faPersonRunning } from '@fortawesome/free-solid-svg-icons';
+import { faHouse, faPersonRunning, faPersonRays } from '@fortawesome/free-solid-svg-icons';
 
 import { MetroContexte } from '../metroContexte';
 import toutesLignes from "../donnees/toutesLignes.json";
@@ -9,8 +9,11 @@ import toutesLignes from "../donnees/toutesLignes.json";
 export default function Portes({ navigation }) {
 
     const { ligneChoisie, setLigneChoisie, setDirection, destination, setDestination, envers, setEnvers } = useContext(MetroContexte);
+    const [combienVoitures, setCombienVoitures] = useState();
+    const [portesConcordia, setPortesConcordia] = useState([]);
 
-    const toutesStations = toutesLignes.find(ligne => ligne.ligne === ligneChoisie).stations;
+    const infoLigne = toutesLignes.find(ligne => ligne.ligne === ligneChoisie);
+    const toutesStations = infoLigne.stations;
     const voitures = toutesStations.find(obj => obj.hasOwnProperty(destination))[destination];
 
     let voiture;
@@ -22,13 +25,99 @@ export default function Portes({ navigation }) {
         setDirection(null);
         setDestination(null);
     }
+
+    useEffect(() => {
+        if (Array.isArray(voiture)) {
+            if (voiture.length > 2) {
+                setCombienVoitures(3)
+            } else {
+                if (Number.isInteger(voiture[0])) {
+                    setCombienVoitures(2)
+                } else {
+                    setCombienVoitures(9)
+                    let portesConc = [];
+                    voiture.forEach(item => {
+                        portesConc.push(item.voiture)
+                    });
+                    setPortesConcordia(portesConc)
+                }
+            }
+        } else {
+            setCombienVoitures(1)
+        }
+    }, [])
     
     return (
         <View style={styles.container}>
-            <View style={styles.infos}>
-                {
-                    Array.isArray(voiture)
-                        ? <>
+            <View style={styles.graph}>
+                <View style={styles.rail}>
+                    <View style={styles.rame}>
+                        {
+                            [...Array(infoLigne.voitures)].map((v, i) =>
+                                <View key={i} style={[styles.voit, { backgroundColor: infoLigne.hex[1] }, i === 0 && styles.premier, i === infoLigne.voitures - 1 && styles.dernier]}>
+                                    {
+                                        combienVoitures === 1 && i + 1 === voiture && <FontAwesomeIcon icon={faPersonRays} />
+                                    }
+                                    {
+                                        (combienVoitures === 2 || combienVoitures === 3) && voiture.includes(i + 1) && <FontAwesomeIcon icon={faPersonRays} />
+                                    }
+                                    {
+                                        combienVoitures === 9 && portesConcordia.includes(i + 1) && <FontAwesomeIcon icon={faPersonRays} />
+                                    }
+                                </View>
+                            )
+                        }
+                    </View>
+                </View>
+            </View>
+            <View style={styles.panneau}>
+                <View style={styles.infos}>
+                    {
+                        combienVoitures === 1 && <Text style={styles.nuit}>Embarquez dans la voiture {voiture}</Text>
+                    }
+                    {
+                        combienVoitures === 2 && <Text style={[styles.nuit, styles.choix2]}>
+                            <Text>Embarquez dans les voitures </Text>
+                            {
+                                voiture.map((item, index) => {
+                                    if (index === 0) {
+                                        return (
+                                            <Text key={index}>{item}</Text>
+                                        )
+                                    } else {
+                                        return (
+                                            <Text key={index}> ou {item}</Text>
+                                        )
+                                    }
+                                })
+                            }
+                        </Text>
+                    }
+                    {
+                        combienVoitures === 3 && <Text style={[styles.nuit, styles.choix2]}>
+                            <Text>Embarquez dans les voitures </Text>
+                            {
+                                voiture.map((item, index) => {
+                                    if (index === voiture.length - 2) {
+                                        return (
+                                            <Text key={index}>{item}</Text>
+                                        )
+                                    } else if (index === voiture.length - 1) {
+                                        return (
+                                            <Text key={index}> ou {item}</Text>
+                                        )
+                                    } else {
+                                        return (
+                                            <Text key={index}>{item}, </Text>
+                                        )
+                                    }
+                                    
+                                })
+                            }
+                        </Text>
+                    }
+                    {
+                        combienVoitures === 9 && <>
                             <Text style={styles.nuit}>Attention! Plusieurs sorties sont possibles.</Text>
                             {
                                 voiture.map((sortie, index) => {
@@ -41,24 +130,69 @@ export default function Portes({ navigation }) {
                                 })
                             }
                         </>
-                        : <Text style={styles.nuit}>Embarquez dans la voiture {voiture}</Text>
-                }
-                <Text style={styles.nuit}>Prochaine station : {destination}</Text>
+                    }
+                    <Text style={styles.nuit}>Votre destination : {destination}</Text>
+                </View>
+                <TouchableOpacity onPress={retour} style={styles.retour}>
+                    <FontAwesomeIcon icon={ faHouse } color='white' />
+                    <Text style={styles.nuit}>Retour</Text>
+                </TouchableOpacity>
             </View>
-            <TouchableOpacity onPress={retour} style={styles.retour}>
-                <FontAwesomeIcon icon={ faHouse } color='white' />
-                <Text style={styles.nuit}>Retour</Text>
-            </TouchableOpacity>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        alignItems: 'center',
         backgroundColor: "#363636",
+        flexDirection: "row",
+        flex: 1
+    },
+    graph: {
         flex: 1,
-        justifyContent: "space-evenly"
+        flexDirection: "row",
+        justifyContent: "center",
+    },
+    rail: {
+        borderColor: "white",
+        borderStyle: "solid",
+        borderLeftWidth: 1,
+        borderRightWidth: 1,
+        justifyContent: "center",
+        width: 30,
+    },
+    rame: {
+        alignItems: "center",
+        flex: 1,
+        marginTop: 20,
+        justifyContent: "center"
+    },
+    voit: {
+        alignItems: "center",
+        backgroundColor: "yellow",
+        borderBottomWidth: 1,
+        borderColor: "white",
+        borderLeftWidth: 2,
+        borderRightWidth: 2,
+        borderTopWidth: 1,
+        height: 60,
+        justifyContent: "center",
+        width: 35,
+    },
+    premier: {
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10
+    },
+    dernier: {
+        borderBottomLeftRadius: 10,
+        borderBottomRightRadius: 10
+    },
+    panneau: {
+        // backgroundColor: "red",
+        alignItems: 'center',
+        flex: 6,
+        justifyContent: "space-evenly",
+        paddingRight: 10
     },
     infos: {
         gap: 10
@@ -67,6 +201,9 @@ const styles = StyleSheet.create({
         alignItems: "center",
         flexDirection: "row",
         gap: 5
+    },
+    choix2: {
+
     },
     retour: {
         alignItems: "center",
